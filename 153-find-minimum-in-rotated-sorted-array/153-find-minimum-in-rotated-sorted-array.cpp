3d808abded283f51a1215fb1428bb8301866368c
Kelvin Eforhare <85578241+Kelvineforhare@users.noreply.github.com>
2022-08-11 14:55:46 +0100

    Time: 16 ms (7.43%), Space: 10.3 MB (23.27%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -3,16 +3,17 @@ public:
     int findMin(vector<int>& nums) {
         int min = 0;
         int max = nums.size()-1;
+        int mid = (max-min)/2;
         while(max-1 > min){
-            printf("min: %d , max: %d\n",nums[min],nums[max]);
-            if(nums[min] > nums[max]){
-                //min = (min+max)/2;
-                min++;
+            printf("min %d , mid %d , max %d\n",nums[min],nums[mid],nums[max]);
+            if(nums[mid] < nums[max]){
+                max = mid;
             }
-            else{
-                max=(min+max)/2;
-                //max++;
+            else if(nums[mid] > nums[min]){
+                min = mid+1;
             }
+            mid = (max+min)/2;
+    
         }
         return (nums[min] < nums[max])?nums[min]:nums[max];
     }
