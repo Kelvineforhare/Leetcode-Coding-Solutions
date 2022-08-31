936659944ec0bd5c2986c4f1b7b1c2de33250a6b
Kelvin Eforhare <85578241+Kelvineforhare@users.noreply.github.com>
2022-08-31 15:55:54 +0100

    Time: 11 ms (47.43%), Space: 9 MB (21.79%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        
+        map<Node*,Node*> visited;
+        return cloneG(node,visited);
+    }
+    
+    Node* cloneG(Node* node, map<Node*,Node*> & visited){
+        if(!node){
+            return nullptr;
+        }
+        Node* newNode = new Node(node->val);
+        visited.insert({node,newNode});
+        vector<Node*> neighborsCopy;
+        for(int i = 0; i < node->neighbors.size();++i){
+            if(visited.find(node->neighbors[i]) == visited.end()){
+                Node* childNode = cloneG(node->neighbors[i],visited);
+                visited.insert({node->neighbors[i],childNode});
+                neighborsCopy.push_back(childNode);
+            }
+            else{
+                neighborsCopy.push_back(visited.find(node->neighbors[i])->second);
+            }
+        }
+        newNode->neighbors = neighborsCopy;
+        return newNode;
+    }
+};
