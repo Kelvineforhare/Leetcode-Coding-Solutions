4df21ce402de2e9cb321f71bd4cac491d926cc6c
Kelvin Eforhare <85578241+Kelvineforhare@users.noreply.github.com>
2022-09-08 15:50:12 +0100

    Time: 1918 ms (5.05%), Space: 235.7 MB (5.08%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        map<int,vector<int>> mp;
+        
+        for(int i = 0; i < prerequisites.size();++i){
+            if(prerequisites[i][1] == prerequisites[i][0]){
+                return false;
+            }
+            
+            vector<int> pre {prerequisites[i][1]};
+            auto insert = mp.insert({prerequisites[i][0],pre});
+            if(!insert.second){
+                insert.first->second.push_back(prerequisites[i][1]);
+            }
+            
+        }
+        
+        
+        set<int> correct;
+        for ( auto it = mp.begin(); it != mp.end(); ++it  ){
+            set<int> visit;
+            // std::cout << (it->first) << '\t';   
+            // std::cout << std::endl;
+            if(!canFin(mp,it->first,visit,correct)){
+                return false;
+            }
+            correct.insert(it->first);
+        }
+        
+        
+    
+        return true;
+    }
+    
+    bool canFin(map<int,vector<int>>& mp,int course,set<int> visit,set<int> & noLoop){
+        //printf("course: %d \n",course);
+        auto in = visit.insert(course); 
+        if(!in.second){
+            return false; 
+        }
+        auto inf = noLoop.insert(course); 
+        if(!inf.second){
+            return true; 
+        }
+        auto find = mp.find(course);
+        if(find == mp.end()){
+            return true;
+        }
+        for(int i = 0; i < find->second.size();++i){
+            if(!canFin(mp,find->second[i],visit,noLoop)){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+//[[0,1],[1,2],[2,0]]
+
+
+        // for ( auto it = mp.begin(); it != mp.end(); ++it  )
+        // {
+        //     std::cout << (it->first) << '\t';
+        //     for(int i = 0; i < it->second.size();++i){
+        //         std::cout << "," << it->second[i] ;
+        //     }   
+        //     std::cout << std::endl;
+        // } 
