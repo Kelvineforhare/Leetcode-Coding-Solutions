f850d374175cdc92516e16b84930620aa81e3dfb
Kelvin Eforhare <85578241+Kelvineforhare@users.noreply.github.com>
2022-09-09 22:24:33 +0100

    Time: 54 ms (5.14%), Space: 10.7 MB (11.53%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set<ListNode*> visit;
+        while(head){
+            auto insert = visit.insert(head);
+            if(!insert.second){
+                return true;
+            }
+            head = head->next;
+        }
+        return false;
+    }
+};
