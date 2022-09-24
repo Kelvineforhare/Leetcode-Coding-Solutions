16745b9ae199690189288a659095d72018e58d68
Kelvin Eforhare <85578241+Kelvineforhare@users.noreply.github.com>
2022-09-24 22:42:47 +0100

    Time: 1039 ms (5.02%), Space: 138.4 MB (5.01%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -8,8 +8,8 @@ public:
             for(int j = 0;j < heights[i].size();++j){
                 set<pair<int,int>> visited;
                 set<pair<int,int>> visited2;
-                printf("i: %d , j: %d\n",i,j);
-                if(canReachAtlantic(i,j,heights,heights[i][j],atlaCache,visited) && canReachPacific(i,j,heights,heights[i][j],pacCache,visited2)){
+                //printf("i: %d , j: %d\n",i,j);
+                if(canReachAtlantic(i,j,heights,atlaCache,visited) && canReachPacific(i,j,heights,pacCache,visited2)){
                     ret.push_back({i,j});
                 }
             }
@@ -17,7 +17,7 @@ public:
         return ret;
     }
     
-    bool canReachPacific(int r,int c,vector<vector<int>>& heights,int size,set <pair<int,int>>& pacCache,set<pair<int,int>>& visited){
+    bool canReachPacific(int r,int c,vector<vector<int>>& heights,set <pair<int,int>>& pacCache,set<pair<int,int>>& visited){
         //printf("r: %d , c: %d , size: %d current: %d \n",r,c,size,heights[r][c]);
         // if(pacCache.find({r,c}) != pacCache.end()){
         //     return true;
@@ -32,28 +32,28 @@ public:
             return true;
         }
         
-        if(r<heights.size()-1 && heights[r+1][c] <= heights[r][c] && canReachPacific(r+1,c,heights,size,pacCache,visited)){
+        if(r<heights.size()-1 && heights[r+1][c] <= heights[r][c] && canReachPacific(r+1,c,heights,pacCache,visited)){
             pacCache.insert({r,c});
             return true;
         }
-        if(c<heights[r].size()-1 && heights[r][c+1] <= heights[r][c] && canReachPacific(r,c+1,heights,size,pacCache,visited)){
-            pacCache.insert({r,c});
-            return true;
-        }
-        
-        if(r>0 && heights[r-1][c] <= heights[r][c] && canReachPacific(r-1,c,heights,size,pacCache,visited)){
+        if(c<heights[r].size()-1 && heights[r][c+1] <= heights[r][c] && canReachPacific(r,c+1,heights,pacCache,visited)){
             pacCache.insert({r,c});
             return true;
         }
         
-        if(c>0 && heights[r][c-1] <= heights[r][c] && canReachPacific(r,c-1,heights,size,pacCache,visited)){
+        if(r>0 && heights[r-1][c] <= heights[r][c] && canReachPacific(r-1,c,heights,pacCache,visited)){
+            pacCache.insert({r,c});
+            return true;
+        }
+        
+        if(c>0 && heights[r][c-1] <= heights[r][c] && canReachPacific(r,c-1,heights,pacCache,visited)){
             pacCache.insert({r,c});
             return true;
         }
         //printf("false\n");
         return false;
     }
-    bool canReachAtlantic(int r,int c,vector<vector<int>>& heights,int size,set <pair<int,int>>& atlaCache,set<pair<int,int>>& visited){ // remove size
+    bool canReachAtlantic(int r,int c,vector<vector<int>>& heights,set <pair<int,int>>& atlaCache,set<pair<int,int>>& visited){ // remove size
         //printf("r: %d , c: %d , size: %d current: %d \n",r,c,size,heights[r][c]);
         // if(atlaCache.find({r,c}) != atlaCache.end()){
         //     return true;
@@ -67,19 +67,19 @@ public:
             atlaCache.insert({r,c});
             return true;
         }
-        if(r<heights.size()-1 && heights[r+1][c] <= heights[r][c] && canReachAtlantic(r+1,c,heights,size,atlaCache,visited)){
+        if(r<heights.size()-1 && heights[r+1][c] <= heights[r][c] && canReachAtlantic(r+1,c,heights,atlaCache,visited)){
             atlaCache.insert({r,c});
             return true;
         }
-        if(c<heights[r].size()-1 && heights[r][c+1] <= heights[r][c] && canReachAtlantic(r,c+1,heights,size,atlaCache,visited)){
+        if(c<heights[r].size()-1 && heights[r][c+1] <= heights[r][c] && canReachAtlantic(r,c+1,heights,atlaCache,visited)){
             atlaCache.insert({r,c});
             return true;
         }
-        if(r>0 && heights[r-1][c] <= heights[r][c] && canReachAtlantic(r-1,c,heights,size,atlaCache,visited)){
+        if(r>0 && heights[r-1][c] <= heights[r][c] && canReachAtlantic(r-1,c,heights,atlaCache,visited)){
             atlaCache.insert({r,c});
             return true;
         }
-        if(c>0 && heights[r][c-1] <= heights[r][c] && canReachAtlantic(r,c-1,heights,size,atlaCache,visited)){
+        if(c>0 && heights[r][c-1] <= heights[r][c] && canReachAtlantic(r,c-1,heights,atlaCache,visited)){
             atlaCache.insert({r,c});
             return true;
         }
