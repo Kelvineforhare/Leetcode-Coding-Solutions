6e068d337917d146460a7c66c3c274e945834b1c
Kelvin Eforhare <85578241+Kelvineforhare@users.noreply.github.com>
2022-09-30 11:02:46 +0100

    Time: 55 ms (27.68%), Space: 11.1 MB (20.51%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int p1 = 0;
+        int p2 = 1;
+        
+        unordered_map<char,int> map;
+        if(s.size() == 0)return 0;
+        int max = 1;
+        map.insert({s[0],0});
+        while(p2 < s.size()){
+            
+            auto insert = map.insert({s[p2],p2});
+            if(!insert.second){
+                if(insert.first->second+1 > p1){
+                    p1 = insert.first->second+1;
+                }
+                insert.first->second = p2;
+                //printf("p1: %d p2: %d \n",p1,p2);
+            }
+            
+            if((p2 - p1)+1 > max){
+                max = (p2 - p1)+1;
+            }
+            ++p2;
+            
+            // for(int i = p1; i < p2;++i){
+            //     printf("%c \n",s[i]);
+            // }
+            //printf("\n");
+        }
+        
+        return max;
+    }
+};
