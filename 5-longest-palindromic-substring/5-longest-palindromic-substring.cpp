4b6a0dc40c70d8077f9452b8cb2a0a11f28807a5
Kelvin Eforhare <85578241+Kelvineforhare@users.noreply.github.com>
2022-10-05 11:53:28 +0100

    Time: 1705 ms (5.01%), Space: 432 MB (5.51%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    
+    string longestPalindrome(string s) {
+        if(s.size() == 0){
+            return "";
+        }
+        string ans = s.substr(0,1);
+        for(int i = 1; i < s.size();++i){
+            int p1 = i -1,p2 = i + 1;
+            while((p1 >=0 && p2 < s.size()) && s[p2] == s[p1]){
+                string temp = s.substr(p1,(p2-p1)+1);
+                if(temp.size() > ans.size()){
+                    ans = temp;
+                }
+                //cout << temp << " p1: " << p1<< " p2: " << p2 << "\n";
+                --p1;
+                ++p2;
+                
+            }
+            p1 = i-1,p2=i;
+            while((p1 >=0 && p2 < s.size()) && s[p2] == s[p1]){
+                string temp = s.substr(p1,(p2-p1)+1);
+                if(temp.size() > ans.size()){
+                    ans = temp;
+                }
+                //cout << temp << " p1: " << p1<< " p2: " << p2 << "\n";
+                --p1;
+                ++p2;
+            }
+            if(ans.size() == s.size()){
+                return ans;
+            }
+            //cout << i << "\n";
+        }
+        return ans;
+    }
+    
+    // bool isPalindrome(string s){
+    //     for(int i =0 , j = s.size()-1;i <= j;++i,--j){
+    //         if(s[i] != s[j]){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+};
