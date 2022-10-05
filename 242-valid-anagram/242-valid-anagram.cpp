24a7732bd45bfb98ab12d4d9aba6e6079de2ca4e
Kelvin Eforhare <85578241+Kelvineforhare@users.noreply.github.com>
2022-10-05 12:53:43 +0100

    Time: 13 ms (70.26%), Space: 7.3 MB (55.92%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        map<char,int> map;
+        for(int i = 0; i < s.size();++i){
+            auto insert = map.insert({s[i],1});
+            if(!insert.second){
+                insert.first->second = insert.first->second + 1;
+            }
+        }
+        for(auto it = map.begin();it!=map.end();++it){
+            cout << it->first << " " << it->second << " \n";
+        }
+        for(int i =0; i < t.size();++i){
+            auto insert = map.insert({t[i],0});
+            if(!insert.second){
+                insert.first->second = insert.first->second - 1;
+            }
+            else{
+                return false;
+            }
+        }
+        for(auto it = map.begin();it!=map.end();++it){
+            cout << it->first << " " << it->second << " \n";
+            if(it->second != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
